8e7f9ec414b13ba845dcf9ed5de9c002fb3d4267
TimLanglois <tim.langlois@uwa.edu.au>
2024-05-10 14:17:16 +0800

    add

--- r/03_tims plot.R
+++ r/03_tims plot.R
@@ -25,14 +25,15 @@ ggplot() +
               method = "lm", se = F, colour = "black", linewidth = 5) +
   labs(x = "Year", y = "Rainfall anomaly (mm)", title = "Winter rainfall anomaly\nSouthwestern Australia (1900 to 2023)") +
   theme_classic() +
-  theme(text = element_text(size = 50)) +
+  theme(text = element_text(size = 55,face = "bold")) +
+  theme(plot.title = element_text(hjust=0.5,face = "bold"))+
   scale_y_continuous(sec.axis = sec_axis(~.x)) +
   scale_x_continuous(sec.axis = sec_axis(~.x, labels = NULL)) +
   annotate(geom = "text", size = 15, x = c(1920, 2000, 1912), 
            y = c(-140, -140, 270),
            label = c("Linear trend of -7.76mm/decade",
                      "Based on 30-year climatology (1961-1990)",
-                     "Australian Burea of Meteorology"))
+                     "Australian Bureau of Meteorology"))
 
 ggsave(filename = "plots/sw-rainfall-anomaly_A0.png",
        height = 841, width = 1189, units = "mm", dpi = 300,
@@ -53,14 +54,15 @@ ggplot() +
               method = "lm", se = F, colour = "black", linewidth = 5) +
   labs(x = "Year", y = "Mean temperature anomaly (°C)", title = "Winter mean temperature anomaly\nSouthwestern Australia (1900 to 2023)") +
   theme_classic() +
-  theme(text = element_text(size = 50)) +
+  theme(text = element_text(size = 55,face = "bold")) +
+  theme(plot.title = element_text(hjust=0.5,face = "bold"))+
   scale_y_continuous(sec.axis = sec_axis(~.x)) +
   scale_x_continuous(sec.axis = sec_axis(~.x, labels = NULL)) +
   annotate(geom = "text", size = 15, x = c(1920, 2000, 1920), 
            y = c(-1.6, -1.6, 1.3),
            label = c("Linear trend of 0.1 °C/decade",
                      "Based on 30-year climatology (1961-1990)",
-                     "Australian Burea of Meteorology"))
+                     "Australian Bureau of Meteorology"))
 
 ggsave(filename = "plots/sw-temperature-anomaly_A0.png",
        height = 841, width = 1189, units = "mm", dpi = 300,
